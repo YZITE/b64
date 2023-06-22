5dd375a19f45a05d83f06c7c7a65073aff62f862
Alain Zscheile <fogti+devel@ytrizja.de>
2023-06-22 20:18:18 +0200

    support for stable base64 version

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 use base64::alphabet::Alphabet;
-use base64::engine::fast_portable::{FastPortable, FastPortableConfig};
+use base64::engine::general_purpose::{GeneralPurpose, GeneralPurposeConfig};
+use base64::engine::DecodePaddingMode;
 use once_cell::sync::Lazy;
 
 pub static B64_ALPHABET: Lazy<Alphabet> = Lazy::new(|| {
     // base64, like URL_SAFE, but '-' is replaced with '+' for better interaction
     // with shell programs, which don't like file names which start with '-'
-    Alphabet::from_str("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+_").unwrap()
+    Alphabet::new("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+_").unwrap()
 });
 
-pub static B64_ENGINE: Lazy<FastPortable> = Lazy::new(|| {
-    FastPortable::from(
+pub static B64_ENGINE: Lazy<GeneralPurpose> = Lazy::new(|| {
+    GeneralPurpose::new(
         &B64_ALPHABET,
-        FastPortableConfig::new().with_encode_padding(false),
+        GeneralPurposeConfig::new()
+            .with_encode_padding(false)
+            .with_decode_padding_mode(DecodePaddingMode::RequireNone),
     )
 });
 
